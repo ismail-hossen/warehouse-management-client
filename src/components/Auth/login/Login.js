260f6e954ad71d49922e3c4dc38c793380c4b160
ismail-hossen <satict2016@gmail.com>
2022-05-02 17:42:04 +0600

    add simple login field without validation

--- src/components/Auth/login/Login.js
+++ src/components/Auth/login/Login.js
@@ -1,26 +1,57 @@
 import React from "react";
-import { Button} from 'react-bootstrap';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Button } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, googleError] = useSignInWithGoogle(auth);
-  
+  const [signInWithGoogle, user, loading] = useSignInWithGoogle(auth);
+  const [signInWithEmail, user2, loading2] =
+    useSignInWithEmailAndPassword(auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    signInWithEmail(data.email, data.password);
+  };
+
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state ? location.state.from?.pathname : "/";
 
-  if (loading) {
-    return <h1>loading...</h1>
-}
-  if (user) {
+  if (loading || loading2) {
+    return <h1>loading...</h1>;
+  }
+  if (user || user2) {
     navigate(from, { replace: true });
-}
+  }
 
   return (
     <div>
-      <Button variant="primary" onClick={() => signInWithGoogle()}>Google</Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Email" {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
+
+        <input
+          placeholder="Password"
+          type="password"
+          {...register("password", { required: true })}
+        />
+        {errors.password && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+
+      <Button variant="primary" onClick={() => signInWithGoogle()}>
+        Google
+      </Button>
     </div>
   );
 };
